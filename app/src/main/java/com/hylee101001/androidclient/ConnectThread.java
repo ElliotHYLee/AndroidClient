bd2d7883adc2ab6ff073512d4f18bef7cd88bfbb
ElliotHYLee <hylee101001@gmail.com>
2017-06-30 15:05:08 -0400

    asdfasd

--- app/src/main/java/com/hylee101001/androidclient/ConnectThread.java
+++ app/src/main/java/com/hylee101001/androidclient/ConnectThread.java
@@ -19,29 +19,30 @@ public class ConnectThread extends Thread {
     public ConnectThread(String addr, int port, TextView tv){
         hostName = addr;
         this.port = port;
-        Log.d("Client Thread", String.valueOf(port));
+        Log.d("Client", "To IP:" + hostName + "\nPort: " + port);
         this.tv = tv;
     }
 
     public void run(){
         try{
-            tv.setText("Trying to Connect1");
+            //tv.setText("Trying to Connect1");
+            Log.d("Client", "Trying to connect1");
+            Log.d("Client", "Client: connectting to " + hostName+":" + String.valueOf(port));
             Socket sock = new Socket(hostName, port);
-            tv.setText("Trying to Connect2");
+            Log.d("Client", "Connection Established");
             ObjectOutputStream outstream = new ObjectOutputStream(sock.getOutputStream());
-            outstream.writeObject("Hello I'm Android");
+            outstream.writeObject("Client: Hello I'm Android");
+            Log.d("Client", "Client: Hello I'm Android");
             outstream.flush();
-            tv.setText("Sent");
-
             ObjectInputStream instream = new ObjectInputStream(sock.getInputStream());
             String obj = (String) instream.readObject();
-
-            Log.d("MainActivity", "Server: " + obj);
-
+            Log.d("Client", "Server: " + obj);
             sock.close();
+            Log.d("Client", "Socket closed");
 
         }catch (Exception e){
             //tv.setText("No response");
+            Log.d("Client", "error ");
             e.printStackTrace();
         }
     }
